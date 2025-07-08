8f071b45471c7b8531d1d839b5130aff36f9fe3f
otaviocostao <dev.otaviocostao@gmail.com>
2025-07-08 16:49:09 -0300

    Files and dependencies

---
+++ src/main/java/com/api/vitreo/VitreoApplication.java
@@ -0,0 +1,13 @@
+package com.api.vitreo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitreoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitreoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/vitreo/VitreoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.vitreo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitreoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
