005a6de86721af0faf84aca093ac9156ee8d267d
otaviocostao <dev.otaviocostao@gmail.com>
2025-08-09 12:34:55 -0300

    Feat: DTO para o response dos endpoints

--- src/main/java/com/api/vitreo/dto/FornecedorResponseDTO.java
+++ src/main/java/com/api/vitreo/dto/FornecedorResponseDTO.java
@@ -1,4 +1,18 @@
 package com.api.vitreo.dto;
 
-public record FornecedorResponseDTO() {
+import com.api.vitreo.entity.Marca;
+
+import java.util.List;
+import java.util.UUID;
+
+public record FornecedorResponseDTO(
+    UUID id,
+    String razaoSocial,
+    String nomeFantasia,
+    String cnpj,
+    String inscricaoEstadual,
+    String telefone,
+    String email,
+    EnderecoDTO endereco
+) {
 }
