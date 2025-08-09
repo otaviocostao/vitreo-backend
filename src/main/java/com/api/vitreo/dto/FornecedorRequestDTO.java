f1daeed92ae05570363eee6337d0a3a3898d103f
otaviocostao <dev.otaviocostao@gmail.com>
2025-08-09 12:33:07 -0300

    Feat: DTO para o request dos endpoints

--- src/main/java/com/api/vitreo/dto/FornecedorRequestDTO.java
+++ src/main/java/com/api/vitreo/dto/FornecedorRequestDTO.java
@@ -1,4 +1,30 @@
 package com.api.vitreo.dto;
 
-public record FornecedorRequestDTO() {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.br.CNPJ;
+
+import java.util.UUID;
+
+public record FornecedorRequestDTO(
+        UUID id,
+
+        @NotBlank(message = "Razão Social é obrigatório")
+        String razaoSocial,
+
+        String nomeFantasia,
+
+        @NotBlank(message = "CNPJ é obrigatório")
+        @CNPJ(message = "CNPJ inválido")
+        String cnpj,
+
+        String inscricaoEstadual,
+
+        String telefone,
+
+        @Email(message = "Email inválido")
+        String email,
+
+        EnderecoDTO endereco
+) {
 }
