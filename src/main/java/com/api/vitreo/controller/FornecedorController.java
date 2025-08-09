3ae135ef78aff6f3be101bc5c74d7759b857a1fe
otaviocostao <dev.otaviocostao@gmail.com>
2025-08-09 12:36:30 -0300

    Feat: Controller de Fornecedor com Endpoints de CRUD

--- src/main/java/com/api/vitreo/controller/FornecedorController.java
+++ src/main/java/com/api/vitreo/controller/FornecedorController.java
@@ -1,4 +1,59 @@
 package com.api.vitreo.controller;
 
+import com.api.vitreo.dto.FornecedorRequestDTO;
+import com.api.vitreo.dto.FornecedorResponseDTO;
+import com.api.vitreo.entity.Fornecedor;
+import com.api.vitreo.service.FornecedorService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/fornecedor")
 public class FornecedorController {
+
+    @Autowired
+    private FornecedorService fornecedorService;
+
+    @PostMapping
+    public ResponseEntity<FornecedorResponseDTO> save(@RequestBody FornecedorRequestDTO requestDTO){
+        FornecedorResponseDTO fornecedor = fornecedorService.saveFornecedor(requestDTO);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(fornecedor.id())
+                .toUri();
+        return ResponseEntity.created(uri).body(fornecedor);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<FornecedorResponseDTO> findById(@RequestParam UUID id,
+                                                          @RequestBody FornecedorRequestDTO requestDTO) {
+        FornecedorResponseDTO fornecedor = fornecedorService.findById(id, requestDTO);
+        return ResponseEntity.ok(fornecedor);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Fornecedor>> findAll() {
+        List<Fornecedor> fornecedores = fornecedorService.findAll();
+        return ResponseEntity.ok(fornecedores);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable("id") UUID id) {
+        fornecedorService.deleteById(id);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<FornecedorResponseDTO> update(@RequestParam("id") UUID id,
+                                                        @Valid @RequestBody FornecedorRequestDTO requestDTO) {
+        FornecedorResponseDTO fornecedor = fornecedorService.update(id, requestDTO);
+        return ResponseEntity.ok(fornecedor);
+    }
 }
