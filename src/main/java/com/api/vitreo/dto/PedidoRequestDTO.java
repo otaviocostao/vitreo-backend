b42e1ed336e28f1bca99f70f13452e4c68f3fff3
otaviocostao <dev.otaviocostao@gmail.com>
2025-08-17 22:58:21 -0300

    Feat: Dto para Request e Response de Pedido

---
+++ src/main/java/com/api/vitreo/dto/PedidoRequestDTO.java
@@ -0,0 +1,21 @@
+package com.api.vitreo.dto;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+public record PedidoRequestDTO(
+        @NotNull UUID clienteId,
+        @NotNull UUID funcionarioId,
+        UUID receituarioId,
+
+        @NotNull @Size(min = 1)
+        List<ItemPedidoRequestDTO> itens,
+
+        LocalDate dataPrevisaoEntrega,
+        BigDecimal desconto
+) {}
---
+++ src/main/java/com/api/vitreo/dto/PedidoResponseDTO.java
@@ -0,0 +1,25 @@
+package com.api.vitreo.dto;
+
+import com.api.vitreo.enums.PedidoStatus;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+public record PedidoResponseDTO(
+        UUID id,
+        Integer ordemServico,
+        PedidoStatus status,
+        LocalDateTime dataPedido,
+        LocalDate dataPrevisaoEntrega,
+        LocalDate dataEntrega,
+        BigDecimal valorArmacao,
+        BigDecimal valorLentes,
+        BigDecimal valorTotal,
+        BigDecimal desconto,
+        BigDecimal valorFinal,
+        ClienteSimplificadoDTO cliente,
+        List<ItemPedidoResponseDTO> itens
+) {}
