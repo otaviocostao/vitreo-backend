0f1053965035d9d3cfe43b759c68156d93af6c97
otaviocostao <dev.otaviocostao@gmail.com>
2025-08-19 22:50:58 -0300

    Feat: Request e Response DTOs para requisições tratadas e respostas sem expor informações desnecessárias

---
+++ src/main/java/com/api/vitreo/components/ReceituarioMapper.java
@@ -0,0 +1,4 @@
+package com.api.vitreo.components;
+
+public class ReceituarioMapper {
+}
--- src/main/java/com/api/vitreo/dto/receituario/ReceituarioResponseDTO.java
+++ src/main/java/com/api/vitreo/dto/receituario/ReceituarioResponseDTO.java
@@ -1,4 +1,29 @@
 package com.api.vitreo.dto.receituario;
 
-public class ReceituarioResponseDTO {
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+public record ReceituarioResponseDTO (
+        UUID id,
+        UUID clienteId,
+        BigDecimal esfericoOd,
+        BigDecimal cilindricoOd,
+        Integer eixoOd,
+        BigDecimal esfericoOe,
+        BigDecimal cilindricoOe,
+        Integer eixoOe,
+        BigDecimal adicao,
+        BigDecimal distanciaPupilar,
+        Double dnpOd,
+        Double dnpOe,
+        Double centroOpticoOd,
+        Double centroOpticoOe,
+        Double anguloMaior,
+        Double ponteAro,
+        Double anguloVertical,
+        String nomeMedico,
+        String crmMedico,
+        LocalDate dataReceita
+) {
 }
