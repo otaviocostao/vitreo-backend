d15a7d5485c2332b35f20992485b3a0199627f92
otaviocostao <dev.otaviocostao@gmail.com>
2025-08-20 15:15:08 -0300

    Feat: DTO para realizar update em Receituario dos campos permitidos

---
+++ src/main/java/com/api/vitreo/dto/receituario/ReceituarioUpdateRequestDTO.java
@@ -0,0 +1,58 @@
+package com.api.vitreo.dto.receituario;
+
+import jakarta.validation.constraints.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+public record ReceituarioUpdateRequestDTO(
+
+        BigDecimal esfericoOd,
+        @DecimalMax(value = "0.0", message = "Cilíndrico deve ser negativo ou zero")
+        BigDecimal cilindricoOd,
+
+        @Min(value = 0, message = "O eixo deve ser no mínimo 0") @Max(value = 180, message = "O eixo deve ser no máximo 180")
+        Integer eixoOd,
+
+        BigDecimal esfericoOe,
+
+        @DecimalMax(value = "0.0", message = "Cilíndrico OD deve ser negativo ou zero")
+        BigDecimal cilindricoOe,
+
+        @Min(value = 0, message = "O eixo deve ser no mínimo 0") @Max(value = 180, message = "O eixo deve ser no máximo 180")
+        Integer eixoOe,
+
+        @DecimalMin(value = "0.0", message = "A adição deve ser positiva")
+        BigDecimal adicao,
+
+        @DecimalMax(value = "0.0", message = "Distância pupilar deve ser positiva")
+        BigDecimal distanciaPupilar,
+
+        @DecimalMin(value = "0.0", message = "A DNP deve ser positiva")
+        Double dnpOd,
+
+        @DecimalMin(value = "0.0", message = "A DNP deve ser positiva")
+        Double dnpOe,
+
+        @DecimalMin(value = "0.0", message = "Centro óptico deve ser positivo")
+        Double centroOpticoOd,
+
+        @DecimalMin(value = "0.0", message = "Centro óptico deve ser positivo")
+        Double centroOpticoOe,
+
+        @DecimalMin(value = "0.0", message = "Medida do Angulo maior deve ser positiva")
+        Double anguloMaior,
+
+        @DecimalMin(value = "0.0", message = "Medida da Ponte e aro deve ser positiva")
+        Double ponteAro,
+
+        @DecimalMin(value = "0.0", message = "Medida do Angulo Vertical deve ser positiva")
+        Double anguloVertical,
+
+        String nomeMedico,
+        String crmMedico,
+
+        LocalDate dataReceita
+) {
+}
