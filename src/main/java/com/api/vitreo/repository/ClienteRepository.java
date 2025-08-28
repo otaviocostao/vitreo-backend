9e9b1e70bf107d68cee31552c8d84974b9feb32c
otaviocostao <dev.otaviocostao@gmail.com>
2025-08-28 18:19:26 -0300

    Fix: Método findAll de ClienteRepository

--- src/main/java/com/api/vitreo/repository/ClienteRepository.java
+++ src/main/java/com/api/vitreo/repository/ClienteRepository.java
@@ -1,6 +1,8 @@
 package com.api.vitreo.repository;
 
 import com.api.vitreo.entity.Cliente;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,5 @@ import java.util.UUID;
 
 public interface ClienteRepository extends JpaRepository<Cliente, UUID> {
 
+    Page<Cliente> findAll(Pageable pageable);
 }
