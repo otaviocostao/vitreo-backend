4dd6d7ed911197ff6b9be9a73290d77abb7e0a94
otaviocostao <dev.otaviocostao@gmail.com>
2025-08-28 18:23:05 -0300

    Feat: Metodo de findByClienteId

--- src/main/java/com/api/vitreo/repository/ReceituarioRepository.java
+++ src/main/java/com/api/vitreo/repository/ReceituarioRepository.java
@@ -3,6 +3,7 @@ package com.api.vitreo.repository;
 import com.api.vitreo.entity.Cliente;
 import com.api.vitreo.entity.Receituario;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +11,5 @@ import java.util.UUID;
 
 @Repository
 public interface ReceituarioRepository extends JpaRepository<Receituario, UUID> {
-    Page<Receituario> findByClienteId(Cliente cliente);
+    Page<Receituario> findByClienteId(Cliente cliente, Pageable pageable);
 }
