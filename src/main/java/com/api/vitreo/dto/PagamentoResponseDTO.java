d59d5a299c65e81a8170a4b628dd240c00e297a9
otaviocostao <dev.otaviocostao@gmail.com>
2025-08-28 23:20:34 -0300

    Feat: DTO para receber o Response de Pagamento

---
+++ src/main/java/com/api/vitreo/dto/PagamentoResponseDTO.java
@@ -0,0 +1,17 @@
+package com.api.vitreo.dto;
+
+import com.api.vitreo.enums.FormaPagamento;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record PagamentoResponseDTO(
+        UUID id,
+        UUID pedidoId,
+        FormaPagamento formaPagamento,
+        BigDecimal valorPago,
+        Integer numeroParcelas,
+        LocalDateTime dataPagamento
+) {
+}
