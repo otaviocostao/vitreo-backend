7bebe8b92e14533b992f1524fb2206e57d7d2f5b
otaviocostao <dev.otaviocostao@gmail.com>
2025-08-28 23:21:13 -0300

    Feat: Método de find pelo Pedido

--- src/main/java/com/api/vitreo/repository/PagamentoRepository.java
+++ src/main/java/com/api/vitreo/repository/PagamentoRepository.java
@@ -1,6 +1,9 @@
 package com.api.vitreo.repository;
 
 import com.api.vitreo.entity.Pagamento;
+import com.api.vitreo.entity.Pedido;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +11,5 @@ import java.util.UUID;
 
 @Repository
 public interface PagamentoRepository extends JpaRepository<Pagamento, UUID> {
+    Page<Pagamento> findAllByPedido(Pedido pedido, Pageable pageable);
 }
