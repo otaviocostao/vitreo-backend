535ad490cadead2941f98f0682a9f6ebadbb8d04
otaviocostao <dev.otaviocostao@gmail.com>
2025-08-28 23:20:54 -0300

    Feat: Mapper para converter Pagamento para entidade e Response

---
+++ src/main/java/com/api/vitreo/components/PagamentoMapper.java
@@ -0,0 +1,32 @@
+package com.api.vitreo.components;
+
+import com.api.vitreo.dto.PagamentoRequestDTO;
+import com.api.vitreo.dto.PagamentoResponseDTO;
+import com.api.vitreo.entity.Pagamento;
+import com.api.vitreo.entity.Pedido;
+
+public class PagamentoMapper {
+
+    public Pagamento toEntity(PagamentoRequestDTO pagamentoRequest, Pedido pedido) {
+        Pagamento pagamento = new Pagamento();
+
+        pagamento.setPedido(pedido);
+        pagamento.setFormaPagamento(pagamentoRequest.formaPagamento());
+        pagamento.setValorPago(pagamentoRequest.valorPago());
+        pagamento.setNumeroParcelas(pagamentoRequest.numeroParcelas());
+        pagamento.setDataPagamento(pagamentoRequest.dataPagamento());
+
+        return pagamento;
+    }
+
+    public PagamentoResponseDTO toResponseDto(Pagamento pagamento){
+        return new PagamentoResponseDTO(
+                pagamento.getId(),
+                pagamento.getPedido().getId(),
+                pagamento.getFormaPagamento(),
+                pagamento.getValorPago(),
+                pagamento.getNumeroParcelas(),
+                pagamento.getDataPagamento()
+        );
+    }
+}
