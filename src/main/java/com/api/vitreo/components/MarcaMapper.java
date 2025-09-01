0b7609457810c54a7893a6a24a5b1bce61314f46
otaviocostao <dev.otaviocostao@gmail.com>
2025-09-01 15:01:22 -0300

    Feat: Mapper para converter Marca em Entity e Request

---
+++ src/main/java/com/api/vitreo/components/MarcaMapper.java
@@ -0,0 +1,18 @@
+package com.api.vitreo.components;
+
+import com.api.vitreo.dto.MarcaRequestDTO;
+import com.api.vitreo.dto.MarcaResponseDTO;
+import com.api.vitreo.entity.Marca;
+
+public class MarcaMapper {
+
+    public Marca toEntity(MarcaRequestDTO marcaRequestDTO) {
+        Marca marca = new Marca();
+        marca.setNome(marcaRequestDTO.nome());
+        return marca;
+    }
+
+    public MarcaResponseDTO toResponseDTO(Marca marca){
+        return new MarcaResponseDTO(marca.getId(), marca.getNome());
+    }
+}
