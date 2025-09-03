6baf64ae93dc29ce6c52a0303ba1c7e81486c297
otaviocostao <dev.otaviocostao@gmail.com>
2025-09-03 15:29:37 -0300

    Feat: Mapper para converter em entidade e dto

---
+++ src/main/java/com/api/vitreo/components/ProdutoMapper.java
@@ -0,0 +1,52 @@
+package com.api.vitreo.components;
+
+import com.api.vitreo.dto.produto.ArmacaoResponseDTO;
+import com.api.vitreo.dto.produto.LenteResponseDTO;
+import com.api.vitreo.dto.produto.ProdutoResponseDTO;
+import com.api.vitreo.entity.Armacao;
+import com.api.vitreo.entity.Lente;
+import com.api.vitreo.entity.Produto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProdutoMapper {
+
+    public ProdutoResponseDTO toResponseDTO(Produto produto) {
+        if (produto instanceof Armacao) {
+            return toArmacaoResponseDTO((Armacao) produto);
+        }
+        if (produto instanceof Lente) {
+            return toLenteResponseDTO((Lente) produto);
+        }
+
+        throw new IllegalArgumentException("Tipo de produto desconhecido: " + produto.getClass().getName());
+    }
+
+    private ArmacaoResponseDTO toArmacaoResponseDTO(Armacao armacao) {
+        return new ArmacaoResponseDTO(
+                armacao.getId(),
+                armacao.getNome(),
+                armacao.getValorVenda(),
+                armacao.getQuantidadeEstoque(),
+                armacao.getMarca() != null ? armacao.getMarca().getNome() : null,
+                armacao.getFornecedor().getRazaoSocial(),
+                armacao.getCor(),
+                armacao.getMaterial(),
+                armacao.getTamanho()
+        );
+    }
+
+    private LenteResponseDTO toLenteResponseDTO(Lente lente) {
+        return new LenteResponseDTO(
+                lente.getId(),
+                lente.getNome(),
+                lente.getValorVenda(),
+                lente.getQuantidadeEstoque(),
+                lente.getMarca() != null ? lente.getMarca().getNome() : null,
+                lente.getFornecedor().getRazaoSocial(),
+                lente.getIndiceRefracao(),
+                lente.getTratamento(),
+                lente.getTipoLente()
+        );
+    }
+}
