0830a5917ceac68aa1bbc2c487c75bc93e52abdb
otaviocostao <dev.otaviocostao@gmail.com>
2025-09-03 15:28:54 -0300

    Feat: Dto para response de lente

---
+++ src/main/java/com/api/vitreo/dto/produto/LenteResponseDTO.java
@@ -0,0 +1,18 @@
+package com.api.vitreo.dto.produto;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public record LenteResponseDTO(
+
+        UUID id,
+        String nome,
+        BigDecimal valorVenda,
+        Integer quantidadeEstoque,
+        String nomeMarca,
+        String nomeFornecedor,
+
+        BigDecimal indiceRefracao,
+        String tratamento,
+        String tipoLente
+) implements ProdutoResponseDTO {}
