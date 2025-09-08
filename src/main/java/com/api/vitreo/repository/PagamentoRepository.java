684187f180a49669ff2987c0a22c471e0f931434
otaviocostao <dev.otaviocostao@gmail.com>
2025-09-08 15:32:32 -0300

    Feat: Método para encontrar o total pago no pedido

--- src/main/java/com/api/vitreo/repository/PagamentoRepository.java
+++ src/main/java/com/api/vitreo/repository/PagamentoRepository.java
@@ -5,11 +5,17 @@ import com.api.vitreo.entity.Pedido;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 @Repository
 public interface PagamentoRepository extends JpaRepository<Pagamento, UUID> {
     Page<Pagamento> findAllByPedido(Pedido pedido, Pageable pageable);
+
+    @Query("SELECT SUM(p.valorPago) FROM Pagamento p WHERE p.pedido.id = :pedidoId")
+    BigDecimal findTotalPagoByPedidoId(@Param("pedidoId") UUID pedidoId);
 }
