a7a885ae5bad1260df478122266f1ff983394dc3
otaviocostao <dev.otaviocostao@gmail.com>
2025-09-09 15:21:48 -0300

    Fix: Adicionando dataPedido, dataEntrega e ordemServico no DTO de pedido

--- src/main/java/com/api/vitreo/dto/pedido/PedidoRequestDTO.java
+++ src/main/java/com/api/vitreo/dto/pedido/PedidoRequestDTO.java
@@ -5,16 +5,20 @@ import jakarta.validation.constraints.Size;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
 public record PedidoRequestDTO(
         @NotNull UUID clienteId,
         UUID receituarioId,
-
+        Integer ordemServico,
         @NotNull @Size(min = 1)
         List<ItemPedidoRequestDTO> itens,
 
+        LocalDateTime dataPedido,
         LocalDate dataPrevisaoEntrega,
+        LocalDate dataEntrega,
         BigDecimal desconto
+
 ) {}
