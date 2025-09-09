b8695a95de4680c2147b1d2443874feb08b0e55c
otaviocostao <dev.otaviocostao@gmail.com>
2025-09-09 15:22:47 -0300

    Fix: Corrigindo marcaProduto para receber somente o nome como String

--- src/main/java/com/api/vitreo/dto/pedido/ItemPedidoResponseDTO.java
+++ src/main/java/com/api/vitreo/dto/pedido/ItemPedidoResponseDTO.java
@@ -1,12 +1,11 @@
 package com.api.vitreo.dto.pedido;
 
-import com.api.vitreo.entity.Marca;
 
 import java.math.BigDecimal;
 
 public record ItemPedidoResponseDTO(
         String nomeProduto,
-        Marca marcaProduto,
+        String marcaProduto,
         Integer quantidade,
         BigDecimal precoUnitario,
         BigDecimal subtotal
