601c205456c8b2d2dc7d8b1e5b1e126d224f3d49
otaviocostao <dev.otaviocostao@gmail.com>
2025-10-09 12:18:37 -0300

    Feat: DTOs simplificados para Fornecedor e Marca

---
+++ src/main/java/com/api/vitreo/dto/fornecedor/FornecedorSimplificadoDTO.java
@@ -0,0 +1,5 @@
+package com.api.vitreo.dto.fornecedor;
+
+import java.util.UUID;
+
+public record FornecedorSimplificadoDTO(UUID id, String razaoSocial) {}
---
+++ src/main/java/com/api/vitreo/dto/marca/MarcaSimplificadaDTO.java
@@ -0,0 +1,5 @@
+package com.api.vitreo.dto.marca;
+
+import java.util.UUID;
+
+public record MarcaSimplificadaDTO(UUID id, String nome) {}
