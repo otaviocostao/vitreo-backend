b6ebaff381dbc6682b5bcac3be355ff4cb3980bc
otaviocostao <dev.otaviocostao@gmail.com>
2025-10-21 14:57:45 -0300

    Feat: Implementando o JpaSpecification no repositoty de Produto para filtrar por nome e tipo

--- src/main/java/com/api/vitreo/repository/ProdutoRepository.java
+++ src/main/java/com/api/vitreo/repository/ProdutoRepository.java
@@ -2,8 +2,9 @@ package com.api.vitreo.repository;
 
 import com.api.vitreo.entity.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.UUID;
 
-public interface ProdutoRepository extends JpaRepository<Produto, UUID> {
+public interface ProdutoRepository extends JpaRepository<Produto, UUID>, JpaSpecificationExecutor<Produto> {
 }
---
+++ src/main/java/com/api/vitreo/repository/ProdutoSpecification.java
@@ -0,0 +1,28 @@
+package com.api.vitreo.repository;
+
+import com.api.vitreo.entity.Produto;
+import com.api.vitreo.enums.TipoProduto;
+import org.springframework.data.jpa.domain.Specification;
+import jakarta.persistence.criteria.Predicate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProdutoSpecification {
+
+    public static Specification<Produto> comFiltros(String nome, TipoProduto tipo) {
+        return (root, query, criteriaBuilder) -> {
+            List<Predicate> predicates = new ArrayList<>();
+
+            if (nome != null && !nome.isEmpty()) {
+                predicates.add(criteriaBuilder.like(criteriaBuilder.lower(root.get("nome")), "%" + nome.toLowerCase() + "%"));
+            }
+
+            if (tipo != null) {
+                predicates.add(criteriaBuilder.equal(root.get("tipoProduto"), tipo));
+            }
+
+            return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
+        };
+    }
+}
