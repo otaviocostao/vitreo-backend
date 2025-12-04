cb6ccf8c1e26cba62404bf8615163a8c7b24a123
otaviocostao <dev.otaviocostao@gmail.com>
2025-12-04 17:51:17 -0300

    Feat (PedidoMapper): Adicionando Receituario e pagamentos dentro do toReponseEntity

--- src/main/java/com/api/vitreo/components/PedidoMapper.java
+++ src/main/java/com/api/vitreo/components/PedidoMapper.java
@@ -1,8 +1,10 @@
 package com.api.vitreo.components;
 
 import com.api.vitreo.dto.cliente.ClienteSimplificadoDTO;
+import com.api.vitreo.dto.pagamento.PagamentoResponseDTO;
 import com.api.vitreo.dto.pedido.ItemPedidoResponseDTO;
 import com.api.vitreo.dto.pedido.PedidoResponseDTO;
+import com.api.vitreo.dto.receituario.ReceituarioResponseDTO;
 import com.api.vitreo.entity.*;
 import org.springframework.stereotype.Component;
 
@@ -23,9 +25,48 @@ public class PedidoMapper {
                 .map(this::toItemPedidoResponseDTO)
                 .collect(Collectors.toList());
 
+        List<PagamentoResponseDTO> pagamentos = pedido.getPagamentos().stream()
+                .map(pagamento -> new PagamentoResponseDTO(
+                        pagamento.getId(),
+                        pagamento.getPedido().getId(),
+                        pagamento.getFormaPagamento(),
+                        pagamento.getValorPago(),
+                        pagamento.getNumeroParcelas(),
+                        pagamento.getDataPagamento()
+                ))
+                .collect(Collectors.toList());
+
+        ReceituarioResponseDTO receituario = null;
+
+        if (pedido.getReceituario() != null) {
+            receituario = new ReceituarioResponseDTO(
+                    pedido.getReceituario().getId(),
+                    pedido.getReceituario().getCliente().getId(),
+                    pedido.getReceituario().getEsfericoOd(),
+                    pedido.getReceituario().getCilindricoOd(),
+                    pedido.getReceituario().getEixoOd(),
+                    pedido.getReceituario().getEsfericoOe(),
+                    pedido.getReceituario().getCilindricoOe(),
+                    pedido.getReceituario().getEixoOe(),
+                    pedido.getReceituario().getAdicao(),
+                    pedido.getReceituario().getDistanciaPupilar(),
+                    pedido.getReceituario().getDnpOd(),
+                    pedido.getReceituario().getDnpOe(),
+                    pedido.getReceituario().getCentroOpticoOd(),
+                    pedido.getReceituario().getCentroOpticoOe(),
+                    pedido.getReceituario().getAnguloMaior(),
+                    pedido.getReceituario().getPonteAro(),
+                    pedido.getReceituario().getAnguloVertical(),
+                    pedido.getReceituario().getNomeMedico(),
+                    pedido.getReceituario().getCrmMedico(),
+                    pedido.getReceituario().getDataReceita()
+            );
+        }
+
         return new PedidoResponseDTO(
                 pedido.getId(),
                 pedido.getOrdemServico(),
+                receituario,
                 pedido.getStatus(),
                 pedido.getDataPedido(),
                 pedido.getDataPrevisaoEntrega(),
@@ -36,7 +77,8 @@ public class PedidoMapper {
                 pedido.getDesconto(),
                 pedido.getValorFinal(),
                 clienteSimplificadoDTO,
-                itensDTO
+                itensDTO,
+                pagamentos
         );
     }
 
