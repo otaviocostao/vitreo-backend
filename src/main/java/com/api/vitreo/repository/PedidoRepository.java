9c16330b962ae860564de301a7f8c315844a755c
otaviocostao <dev.otaviocostao@gmail.com>
2025-12-11 19:19:45 -0300

    Feat (Pedido): Adicionando queries no repository para as métricas como quantidade de vendas, valor total das vendas e ultimas vendas do Dashboard do site

--- src/main/java/com/api/vitreo/repository/PedidoRepository.java
+++ src/main/java/com/api/vitreo/repository/PedidoRepository.java
@@ -1,14 +1,18 @@
 package com.api.vitreo.repository;
 
-import com.api.vitreo.entity.Cliente;
 import com.api.vitreo.entity.Pedido;
 import com.api.vitreo.enums.PedidoStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -19,4 +23,18 @@ public interface PedidoRepository extends JpaRepository<Pedido, UUID>, JpaSpecif
     Page<Pedido> findAllByStatus(PedidoStatus pedidoStatus, Pageable pageable);
 
     Optional<Pedido> findByOrdemServico(Integer ordemServico);
+
+    Long countByDataPedidoBetween(LocalDateTime inicio, LocalDateTime fim);
+
+    @Query("SELECT SUM(p.valorFinal) FROM Pedido p WHERE p.dataPedido BETWEEN :inicio AND :fim")
+    Optional<BigDecimal> sumValorFinalByDataPedidoBetween(@Param("inicio") LocalDateTime inicio, @Param("fim") LocalDateTime fim);
+
+    @Query("SELECT FUNCTION('DATE', p.dataPedido), SUM(p.valorFinal) " +
+            "FROM Pedido p " +
+            "WHERE p.dataPedido BETWEEN :inicio AND :fim " +
+            "GROUP BY FUNCTION('DATE', p.dataPedido) " +
+            "ORDER BY FUNCTION('DATE', p.dataPedido)")
+    List<Object[]> findReceitaPorDiaAsObjectArray(@Param("inicio") LocalDateTime inicio, @Param("fim") LocalDateTime fim);
+
+    List<Pedido> findTop4ByOrderByDataPedidoDesc();
 }
