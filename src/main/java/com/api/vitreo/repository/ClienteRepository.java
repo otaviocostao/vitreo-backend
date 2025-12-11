0b2296c3650979ab372fd5033300364039d83327
otaviocostao <dev.otaviocostao@gmail.com>
2025-12-11 19:20:21 -0300

    Feat (Cliente): Adicionando uma query na repository de Cliente para contar os cadastro de clientes dentro do prazo solicitado

--- src/main/java/com/api/vitreo/repository/ClienteRepository.java
+++ src/main/java/com/api/vitreo/repository/ClienteRepository.java
@@ -6,10 +6,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 
 public interface ClienteRepository extends JpaRepository<Cliente, UUID>, JpaSpecificationExecutor<Cliente> {
 
     Page<Cliente> findAll(Pageable pageable);
+
+    Long countByDataCadastroBetween(LocalDateTime inicio, LocalDateTime fim);
+
 }
